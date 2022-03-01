cd3e9583f494635241e23e34c233e66ef713876f
Eytan Zury <eytanzury@gmail.com>
2022-03-01 15:14:58 +0200

    Added a very cool new event!!

---
+++ custom_events/Camera Tween Pos.lua
@@ -0,0 +1,16 @@
+function Split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch('(.-)'..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+function onEvent(name, value1, value2)
+	if name == 'Camera Tween Pos' then
+		XYAndDir = Split(tostring(value1), ', ');
+		doTweenX('CameraEventX', 'camFollowPos', tonumber(XYAndDir[1]), tonumber(XYAndDir[3]), tostring(value2));
+		doTweenY('CameraEventY', 'camFollowPos', tonumber(XYAndDir[2]), tonumber(XYAndDir[3]), tostring(value2));
+        setProperty('isCameraOnForcedPos', true);
+	end
+end
