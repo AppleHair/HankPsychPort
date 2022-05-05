522a8a120cbc5b7f260137e0f0d4a2fb4b0c4091
Eytan Zury <eytanzury@gmail.com>
2022-05-05 20:19:44 +0300

    FINISHED TRICKY STATIC

--- custom_events/Start Tricky Static.lua
+++ custom_events/Start Tricky Static.lua
@@ -4,23 +4,25 @@ function onCreate()
 	setGraphicSize('Static',1280,720);
 	setObjectCamera('Static','camHud');
 	setProperty('Static.visible', false);
-	setProperty('Static.alpha', 0.7);
+	setProperty('Static.alpha', 0.5);
 	precacheImage('TrickyStatic');
     addLuaSprite('Static', false);
 	
 	makeLuaText('TrickyText', 'SUSSY BAKA', 1280, 0, 0);
 	setTextItalic('TrickyText', true);
-	setTextSize('TrickyText', 110);
+	setTextSize('TrickyText', 150);
 	setTextBorder('TrickyText', 0, 0);
 	setTextFont('TrickyText', 'impact.ttf');
 	setTextColor('TrickyText', '0xff0000');
 	setTextAlignment('TrickyText', 'center');
+	setObjectCamera('TrickyText','camGame');
 	setProperty('TrickyText.visible', false);
 	addLuaText('TrickyText');
+	doTweenAngle('TextRotRight', 'TrickyText', 2, 0.1, 'quadIn');
 end
 
 
-local strings = {'SUS','SUSSY','AMOGUS','IMPASTA','SUSSY BAKA'};
+local strings = {'IMPROBABLE','HANK!!!','MADNESS',"WHO'S THAT??",'INTERRUPTION','FIGHT ME','INVALID','CORRECTION','CLOWN'};
 local minY = 200;
 local maxY = 400;
 
@@ -40,22 +42,24 @@ end
 
 function onEvent(name, value1, value2)
     if name == 'Start Tricky Static' then
-        doTheThing = true;
 		if tostring(value1) == 'true' then
-        	DoTheStaticTrickyThing('CLOWN ENGAGED', 50, 350);
+        	DoTheStaticTrickyThing('CLOWN ENGAGED!', 50, 375);
+			runTimer('StartStatic');
+			return;
 		end
+		doTheThing = true;
     end
     if name == 'Stop Tricky Static' and doTheThing then
         doTheThing = false;
     end
 	if name == 'Do a HAY!!! static' and not doTheThing then
-        DoTheStaticTrickyThing('HAY!!!', -100, 250);
+        DoTheStaticTrickyThing('HAY!!!', 200, 275);
     end
 end
 
 -- The thing itself:
 function onStepHit()
-	if doTheThing and getRandomBool(6) then
+	if doTheThing and getRandomBool(5) then
         DoTheStaticTrickyThing(strings[getRandomInt(1, table.getn(strings))], getRandomInt(minX, maxX), getRandomInt(minY, maxY));
     end
 end
@@ -65,4 +69,19 @@ function onSoundFinished(tag)
         setProperty('Static.visible', false);
 		setProperty('TrickyText.visible', false);
     end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'StartStatic' then
+		doTheThing = true;
+	end
+end
+
+function onTweenCompleted(tag)
+	if tag == 'TextRotRight' then
+		doTweenAngle('TextRotLeft', 'TrickyText', -2, 0.01, 'quadIn');
+	end
+	if tag == 'TextRotLeft' then
+		doTweenAngle('TextRotRight', 'TrickyText', 2, 0.01, 'quadIn');
+	end
 end
