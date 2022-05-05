9ef3f0815cbe59ec94a680677e218aea750752dd
Eytan Zury <eytanzury@gmail.com>
2022-05-05 18:31:36 +0300

    more progress on tricky static

--- custom_events/Start Tricky Static.lua
+++ custom_events/Start Tricky Static.lua
@@ -4,36 +4,65 @@ function onCreate()
 	setGraphicSize('Static',1280,720);
 	setObjectCamera('Static','camHud');
 	setProperty('Static.visible', false);
+	setProperty('Static.alpha', 0.7);
 	precacheImage('TrickyStatic');
     addLuaSprite('Static', false);
+	
+	makeLuaText('TrickyText', 'SUSSY BAKA', 1280, 0, 0);
+	setTextItalic('TrickyText', true);
+	setTextSize('TrickyText', 110);
+	setTextBorder('TrickyText', 0, 0);
+	setTextFont('TrickyText', 'impact.ttf');
+	setTextColor('TrickyText', '0xff0000');
+	setTextAlignment('TrickyText', 'center');
+	setProperty('TrickyText.visible', false);
+	addLuaText('TrickyText');
 end
 
+
+local strings = {'SUS','SUSSY','AMOGUS','IMPASTA','SUSSY BAKA'};
+local minY = 200;
+local maxY = 400;
+
+local minX = -200;
+local maxX = 200;
+
+
 local doTheThing = false;
-function DoTheStaticTrickyThing(text)
+function DoTheStaticTrickyThing(text, x, y)
+	setTextString('TrickyText', text);
+	setProperty('TrickyText.x', x);
+	setProperty('TrickyText.y', y);
+	setProperty('TrickyText.visible', true);
     setProperty('Static.visible', true);
-    runTimer('StopStat', 0.01);
+    playSound('staticSound', 1, 'staticSound');
 end
 
 function onEvent(name, value1, value2)
     if name == 'Start Tricky Static' then
         doTheThing = true;
-        DoTheStaticTrickyThing('CLOWN ENGAGED');
+		if tostring(value1) == 'true' then
+        	DoTheStaticTrickyThing('CLOWN ENGAGED', 50, 350);
+		end
     end
     if name == 'Stop Tricky Static' and doTheThing then
-        DoTheStaticTrickyThing('HEY!!');
         doTheThing = false;
+    end
+	if name == 'Do a HAY!!! static' and not doTheThing then
+        DoTheStaticTrickyThing('HAY!!!', -100, 250);
     end
 end
 
 -- The thing itself:
-function StepHit(id, direction, noteType, isSustainNote)
-	if doTheThing and getRandomBool(20) then
-        DoTheStaticTrickyThing('');
+function onStepHit()
+	if doTheThing and getRandomBool(6) then
+        DoTheStaticTrickyThing(strings[getRandomInt(1, table.getn(strings))], getRandomInt(minX, maxX), getRandomInt(minY, maxY));
     end
 end
 
-function onTimerCompleted(tag, loops, loopsLeft)
-	if tag = 'StopStat' then
+function onSoundFinished(tag)
+	if tag == 'staticSound' then
         setProperty('Static.visible', false);
+		setProperty('TrickyText.visible', false);
     end
 end
