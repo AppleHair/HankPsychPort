8d459f4ccdc83f14b9da74b4b6048b64fd446fb9
AppleHair <eytanzury@gmail.com>
2024-02-21 17:36:36 +0200

    tried to fix the game over screen for 0.7.1h

--- scripts/VS GameOver.lua
+++ scripts/VS GameOver.lua
@@ -1,19 +1,44 @@
+
+local inPsych071h = false;
+
 function onGameOverStart()
     addHaxeLibrary('GameOverSubstate', 'substates');
     runHaxeCode([[
         var bf = GameOverSubstate.instance.boyfriend;
         var follow = GameOverSubstate.instance.camFollow;
 
+        // the follow position will be
+        // determined by the boyfriend's
+        // midpoint on the "deathLoop" animation
         bf.playAnim('deathLoop');
         follow.setPosition(bf.getGraphicMidpoint().x, bf.getGraphicMidpoint().y);
         bf.playAnim('firstDeath');
 
         FlxG.camera.follow(follow);
-        camera.followLerp = 3;
-
-        GameOverSubstate.instance.moveCamera = true;
     ]]);
+
+    -- check if we're in version 0.7.1h
+    inPsych071h = version:find('^v?0%.7%.1') ~= nil;
+
+    -- the way we change the follow lerp in each version is different
+    if inPsych071h then
+        setProperty('isFollowingAlready', true);
+        setProperty('updateCamera', false);
+        setPropertyFromClass('flixel.FlxG', 'camera.followLerp', 0);
+    else
+        setProperty('moveCamera', true);
+        setPropertyFromClass('flixel.FlxG', 'camera.followLerp', 3);
+    end
 
     -- we make the camera zoom smaller to make the game over screen more accurate to FNF ONLINE VS.
     setPropertyFromClass('flixel.FlxG', 'camera.zoom', 0.65);
+end
+
+function onUpdatePost(elapsed)
+    if not (inGameOver and inPsych071h) then
+        return;
+    end
+    -- we force our own follow lerp (only in 0.7.1h)
+    setPropertyFromClass('flixel.FlxG', 'camera.followLerp', elapsed * 3 /
+    (getPropertyFromClass('flixel.FlxG', 'updateFramerate') / 60));
 end
