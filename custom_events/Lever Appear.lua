645110fc51e1e6f9feff13fe059b0f271b8499d9
AppleHair <eytanzury@gmail.com>
2024-02-24 16:28:24 +0200

    made lever mechanic

--- custom_events/Lever Appear.lua
+++ custom_events/Lever Appear.lua
@@ -15,4 +15,46 @@ function onCreate()
         close();
         return;
     end
+    -- if we don't need balance,
+    -- we'll create the lever.
+    makeAnimatedLuaSprite('lever', 'Lever', 395, 405);
+    addAnimationByPrefix('lever', 'appear', 'Appear', 24, false);
+    addAnimationByPrefix('lever', 'pull', 'Pull', 24, false);
+    addAnimationByIndicesLoop('lever', 'pull-loop', 'Pull', "11,12,13,14,15", 24);
+    scaleObject('lever', 0.75, 0.75, false);
+    setProperty('lever.alpha', 0.00001);
+    setObjectCamera('lever', "camHUD");
+    addOffset('lever', 'appear', 0, -14);
+    addOffset('lever', 'pull', 0, 0);
+    addOffset('lever', 'pull-loop', 0, 0);
+    addLuaSprite('lever', false);
+end
+
+-- tells if the health bar
+-- should be drained or not.
+local drain = false;
+
+function onUpdatePost(elapsed)
+    -- the health bar should be drained
+    -- when the lever is pulled.
+    if getProperty('lever.animation.curAnim.finished') then
+        if getProperty('lever.animation.curAnim.name') == 'appear' then
+            playAnim('lever', 'pull');
+        elseif getProperty('lever.animation.curAnim.name') == 'pull' then
+            playAnim('lever', 'pull-loop');
+            drain = true;
+        end
+    end
+
+    if drain then
+        setProperty('health', getProperty('health') - elapsed * 0.1);
+    end
+end
+
+function onEvent(event)
+    if event == "Lever Appear" then
+        playAnim('lever', 'appear', true);
+        -- setProperty('lever.visible', true);
+        setProperty('lever.alpha', 1);
+    end
 end
