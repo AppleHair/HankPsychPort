8a612c6fe5d0d8e9f8f73bbe038bf6e360003e3d
AppleHair <eytanzury@gmail.com>
2024-02-29 18:53:56 +0200

    balanced shake and zoom intensitys

--- custom_notetypes/Bullet Note.lua
+++ custom_notetypes/Bullet Note.lua
@@ -244,8 +244,8 @@ function onUpdatePost(elapsed)
 			-- once per bullet note strumtime)
 			triggerEvent("Signal-Bullet Note Time");
 			-- shake camera
-			cameraShake('game', 0.005, 0.07);
-			triggerEvent('Add Camera Zoom', 0.01, 0);
+			cameraShake('game', 0.007, 0.07);
+			triggerEvent('Add Camera Zoom', 0.007, 0);
 		end
 		-- if you want to override the
 		-- current sing animation with
